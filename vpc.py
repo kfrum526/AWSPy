3c1f0b66d543505c02dd06b5b66416414a783021
kfrum526 <kfrum4@gmail.com>
2022-10-12 19:32:23 -0400

    fixed funtion issue

--- vpc.py
+++ vpc.py
@@ -11,18 +11,18 @@ ec2=boto3.client('ec2')
 vpcname = input('What is the name of your VPC: ')
 
 
-# def newvpc(newvpc):
-#     ec2.create_vpc(
-#         CidrBlock="10.0.0.0/16",
-#         AmazonProvidedIpv6CidrBlock=False,
-#         DryRun=True,
-#         TagSpecifications=[
-#             {
-#             'ResourceType': 'vpc',
-#             'Tags': [{'Key': 'Name', 'Value': newvpc}]
-#             }
-#         ]  
-#     )
+def newvpc(newvpc):
+    ec2.create_vpc(
+        CidrBlock="10.0.0.0/16",
+        AmazonProvidedIpv6CidrBlock=False,
+        DryRun=True,
+        TagSpecifications=[
+            {
+            'ResourceType': 'vpc',
+            'Tags': [{'Key': 'Name', 'Value': newvpc}]
+            }
+        ]  
+    )
 
 myvpc = ec2.describe_vpcs(
         Filters=[
