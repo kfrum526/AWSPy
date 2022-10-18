ff83ad851f9a8e0f2a89485befce35428e56be39
Kevin Frum <kfrum4@gmail.com>
2022-10-17 21:05:20 -0400

    removed lines

--- account.py
+++ account.py
@@ -6,23 +6,3 @@ from botocore.config import Config
 my_config = Config(
     region_name = 'us-east-1',
 )
-
-
-#     igwd = ec2.describe_internet_gateways(
-#     Filters=[
-#         {
-#             'Name': 'tag:Name',
-#             'Values': ['PythonIGW']
-#         }
-#     ]
-# )
-# respig = igwd['InternetGateways']
-
-# for i in respig:
-#     igwid=i['InternetGatewayId']
-
-
-# ec2.attach_internet_gateway(
-#     InternetGatewayId=igwid,
-#     VpcId=idvpc
-#     )
